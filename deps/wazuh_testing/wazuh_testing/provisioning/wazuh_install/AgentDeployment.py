d0fd8c0443f89ce1851fa5efbc8333a69e1af23d
Jotacarma <jotacarma90@gmail.com>
2021-07-28 17:40:28 +0200

    refac: Two new functions to checks install

--- deps/wazuh_testing/wazuh_testing/provisioning/wazuh_install/AgentDeployment.py
+++ deps/wazuh_testing/wazuh_testing/provisioning/wazuh_install/AgentDeployment.py
@@ -5,31 +5,76 @@ from wazuh_testing.provisioning.ansible.AnsibleRunner import AnsibleRunner
 
 
 class AgentDeployment(WazuhDeployment):
+    """Deploy Wazuh agent with all the elements needed, set from the configuration file
+
+    Args:
+        installation_files (string): Path where is located the Wazuh instalation files.
+        configuration (WazuhConfiguration): Configuration object to be set.
+        inventory_file_path (string): Path where is located the ansible inventory file.
+        install_mode (string): 'package' or 'sources' installation mode.
+        install_dir_path (string): Path where the Wazuh installation will be stored.
+        hosts (string): Group of hosts to be deployed.
+        ip_server (string): Manager IP to connect.
+
+    Attributes:
+        installation_files (string): Path where is located the Wazuh instalation files.
+        configuration (WazuhConfiguration): Configuration object to be set.
+        inventory_file_path (string): Path where is located the ansible inventory file.
+        install_mode (string): 'package' or 'sources' installation mode.
+        install_dir_path (string): Path where the Wazuh installation will be stored.
+        hosts (string): Group of hosts to be deployed.
+        ip_server (string): Manager IP to connect.
+    """
 
     def install(self):
+        """Child method to install Wazuh in agent
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         super().install('agent')
 
     def start_service(self):
+        """Child method to start service in agent
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         super().start_service('agent')
 
     def restart_service(self):
+        """Child method to restart service in agent
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         super().restart_service('agent')
 
     def stop_service(self):
+        """Child method to stop service in agent
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         super().stop_service('agent')
 
     def register_agent(self):
+        """Set the manager ip in the ossec.conf, and restart Wazuh agent to let autoenrollment register it.
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         tasks_list = []
 
         tasks_list.append(AnsibleTask({'name': 'Configuring server ip to autoenrollment agent',
-                                       'lineinfile': {'path': f'{self.install_dir}/etc/ossec.conf',
+                                       'lineinfile': {'path': f'{self.install_dir_path}/etc/ossec.conf',
                                                       'regexp': '<address>(.*)</address>',
                                                       'line': f'<address>{self.ip_server}</address>',
                                                       'backrefs': 'yes'},
                                        'when': 'ansible_system != "Windows"'}))
 
         tasks_list.append(AnsibleTask({'name': 'Configuring server ip to autoenrollment agent',
-                                       'lineinfile': {'path': f'{self.install_dir}\\ossec.conf',
+                                       'lineinfile': {'path': f'{self.install_dir_path}\\ossec.conf',
                                                       'regexp': '<address>(.*)</address>',
                                                       'line': f'<address>{self.ip_server}</address>',
                                                       'backrefs': 'yes'},
@@ -38,5 +83,31 @@ class AgentDeployment(WazuhDeployment):
         playbook_parameters = {'tasks_list': tasks_list, 'hosts': self.hosts, 'gather_facts': True, 'become': True}
 
         self.stop_service()
-        AnsibleRunner.run_ephemeral_tasks(self.inventory, playbook_parameters)
+
+        output = AnsibleRunner.run_ephemeral_tasks(self.inventory_file_path, playbook_parameters)
+
         self.start_service()
+
+        return output
+
+    def health_check(self):
+        """Check if the installation is full complete, and the necessary items are ready
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
+        super().health_check()
+
+        tasks_list = []
+        tasks_list.append(AnsibleTask({'name': 'Extract service status',
+                                       'command': f'{self.install_dir}/bin/wazuh-control status',
+                                       'when': 'ansible_system != "Windows"',
+                                       'register': 'status'}))
+
+        tasks_list.append(AnsibleTask({'name': 'Check services',
+                                       'failed_when': ['"wazuh-agentd" not in status.stdout',
+                                                       '"wazuh-execd" not in status.stdout']}))
+
+        playbook_parameters = {'tasks_list': tasks_list, 'hosts': self.hosts, 'gather_facts': True, 'become': True}
+
+        return AnsibleRunner.run_ephemeral_tasks(self.inventory_file_path, playbook_parameters)
--- deps/wazuh_testing/wazuh_testing/provisioning/wazuh_install/WazuhDeployment.py
+++ deps/wazuh_testing/wazuh_testing/provisioning/wazuh_install/WazuhDeployment.py
@@ -2,25 +2,44 @@
 from abc import ABC, abstractmethod
 from wazuh_testing.provisioning.ansible.AnsibleTask import AnsibleTask
 from wazuh_testing.provisioning.ansible.AnsibleRunner import AnsibleRunner
-import sys
 
 
 class WazuhDeployment(ABC):
+    """Deploy Wazuh with all the elements needed, set from the configuration file
 
-    def __init__(self, installation_files, system, configuration=None, inventory='/tmp/inventory.yaml',
-                 install_mode='sources', install_dir='/var/ossec', ip_server=None, hosts='all',
-                 services=[]):
+    Args:
+        installation_files (string): Path where is located the Wazuh instalation files.
+        configuration (WazuhConfiguration): Configuration object to be set.
+        inventory_file_path (string): Path where is located the ansible inventory file.
+        install_mode (string): 'package' or 'sources' installation mode.
+        install_dir_path (string): Path where the Wazuh installation will be stored.
+        hosts (string): Group of hosts to be deployed.
+
+    Attributes:
+        installation_files (string): Path where is located the Wazuh instalation files.
+        configuration (WazuhConfiguration): Configuration object to be set.
+        inventory_file_path (string): Path where is located the ansible inventory file.
+        install_mode (string): 'package' or 'sources' installation mode.
+        install_dir_path (string): Path where the Wazuh installation will be stored.
+        hosts (string): Group of hosts to be deployed.
+    """
+    def __init__(self, installation_files, configuration=None, inventory_file_path='/tmp/inventory.yaml',
+                 install_mode='package', install_dir_path='/var/ossec', hosts='all', ip_server=None):
         self.installation_files = installation_files
         self.configuration = configuration
-        self.inventory = inventory
+        self.inventory_file_path = inventory_file_path
         self.install_mode = install_mode
-        self.install_dir = install_dir
-        self.ip_server = ip_server
+        self.install_dir_path = install_dir_path
         self.hosts = hosts
-        self.services = services
+        self.ip_server = ip_server
 
     @abstractmethod
     def install(self, install_type):
+        """Installs Wazuh (agent or manager) by creating an ansible playbook and launching it
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         tasks_list = []
         if self.install_mode == 'sources':
             tasks_list.append(AnsibleTask({
@@ -38,13 +57,13 @@ class WazuhDeployment(ABC):
 
             tasks_list.append(AnsibleTask({
                 'name': 'Render the "preloaded-vars.conf" file',
-                'template': {'src': 'wazuh_testing/provisioning/wazuh_install/templates/preloaded_vars.conf.j2',
+                'template': {'src': '/home/vhalgarv/drawers/git/wazuh-qa/deps/wazuh_testing/wazuh_testing/provisioning/wazuh_install/templates/preloaded_vars.conf.j2',
                              'dest': f'{self.installation_files}/etc/preloaded-vars.conf',
                              'owner': 'root',
                              'group': 'root',
                              'mode': '0644'},
                 'vars': {'install_type': install_type,
-                         'install_dir': f'{self.install_dir}',
+                         'install_dir': f'{self.install_dir_path}',
                          'ip_server': f'{self.ip_server}',
                          'ca_store': f'{self.installation_files}/wpk_root.pem',
                          'make_cert': 'y' if install_type == 'server' else 'n'},
@@ -88,9 +107,14 @@ class WazuhDeployment(ABC):
 
         playbook_parameters = {'tasks_list': tasks_list, 'hosts': self.hosts, 'gather_facts': True, 'become': True}
 
-        AnsibleRunner.run_ephemeral_tasks(self.inventory, playbook_parameters)
+        return AnsibleRunner.run_ephemeral_tasks(self.inventory_file_path, playbook_parameters)
 
     def __control_service(self, command, install_type):
+        """Private method to control the Wazuh service in different systems.
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         tasks_list = []
         service_name = install_type if install_type == 'agent' else 'manager'
         service_command = f'{command}ed' if command != 'stop' else 'stopped'
@@ -105,7 +129,7 @@ class WazuhDeployment(ABC):
 
         tasks_list.append(AnsibleTask({'name': f'Wazuh agent {command} service',
                                        'become': True,
-                                       'command': f'{self.install_dir}/bin/wazuh-control {command}',
+                                       'command': f'{self.install_dir_path}/bin/wazuh-control {command}',
                                        'when': 'ansible_system == "darwin" or ansible_system == "SunOS" or ' +
                                                'output_command.failed == true'}))
 
@@ -117,16 +141,80 @@ class WazuhDeployment(ABC):
 
         playbook_parameters = {'tasks_list': tasks_list, 'hosts': self.hosts, 'gather_facts': True, 'become': True}
 
-        AnsibleRunner.run_ephemeral_tasks(self.inventory, playbook_parameters)
+        return AnsibleRunner.run_ephemeral_tasks(self.inventory_file_path, playbook_parameters)
 
     @abstractmethod
     def start_service(self, install_type):
+        """Abstract method to start service
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         self.__control_service('start', install_type)
 
     @abstractmethod
     def restart_service(self, install_type):
+        """Abstract method to restart service
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         self.__control_service('restart', install_type)
 
     @abstractmethod
     def stop_service(self, install_type):
+        """Abstract method to stop service
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
         self.__control_service('stop', install_type)
+
+    @abstractmethod
+    def health_check(self):
+        """Check if the installation is full complete, and the necessary items are ready
+
+        Returns:
+            AnsibleOutput: Result of the ansible playbook run.
+        """
+        tasks_list = []
+        tasks_list.append(AnsibleTask({'name': 'Read ossec.log searching errors',
+                                       'lineinfile': {'path': f'{self.install_dir_path}/logs/ossec.log',
+                                                      'line': 'ERROR|CRITICAL'},
+                                       'when': 'ansible_system != "Windows"',
+                                       'register': 'exists',
+                                       'check_mode': 'yes',
+                                       'failed_when': 'exists is not changed'}))
+
+        tasks_list.append(AnsibleTask({'name': 'Read ossec.log searching errors',
+                                       'lineinfile': {'path': f'{self.install_dir_path}/ossec.log',
+                                                      'line': 'ERROR'},
+                                       'when': 'ansible_system == "Windows"',
+                                       'register': 'exists',
+                                       'check_mode': 'yes',
+                                       'failed_when': 'exists is not changed'}))
+
+        playbook_parameters = {'tasks_list': tasks_list, 'hosts': self.hosts, 'gather_facts': True, 'become': True}
+
+        return AnsibleRunner.run_ephemeral_tasks(self.inventory_file_path, playbook_parameters)
+
+    def wazuh_is_already_installed(self):
+        """Check if Wazuh is installed in the system
+
+        Returns:
+            bool: True if wazuh is already installed, False if not
+        """
+        tasks_list = []
+        tasks_list.append(AnsibleTask({'name': 'Check Wazuh directory exist',
+                                       'stat': {'path': f'{self.install_dir_path}'},
+                                       'register': 'dir_exist',
+                                       'failed_when': 'dir_exist.stat.exists and dir_exist.stat.isdir'}))
+
+        playbook_parameters = {'tasks_list': tasks_list, 'hosts': self.hosts, 'gather_facts': True, 'become': True}
+
+        output = AnsibleRunner.run_ephemeral_tasks(self.inventory_file_path, playbook_parameters, raise_on_error=False)
+
+        if output.rc == 0:
+            return False
+        else:
+            return True
