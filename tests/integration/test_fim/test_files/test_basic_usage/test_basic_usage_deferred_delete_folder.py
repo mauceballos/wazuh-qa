2c2514e7022a5f1083ac88d3baf3cab642c55d48
fedepacher <fede_pacher@hotmail.com>
2021-10-19 20:52:39 -0300

    eliminacion de los prints

--- tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_deferred_delete_folder.py
+++ tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_deferred_delete_folder.py
@@ -10,7 +10,7 @@ from json import JSONDecodeError
 
 import pytest
 
-from wazuh_testing import global_parameters, logger
+from wazuh_testing import global_parameters
 from wazuh_testing.fim import LOG_FILE_PATH, generate_params, create_file, REGULAR, callback_detect_event
 from wazuh_testing.tools import PREFIX
 from wazuh_testing.tools.configuration import load_wazuh_configurations, check_apply_test
@@ -41,52 +41,25 @@ configurations = load_wazuh_configurations(configurations_path, __name__, params
 # callback
 def callback_detect_delete_event(line):
     msg = r'.*Sending FIM event: (.+)$'
-    print(type(msg))
-    print(msg)
-    print("\n\n")
-    print("########################################################\n")
-   
-    print(type(line))
-    print(line)
-    print("\n\n")
-    print("########################################################\n")
-
     match = re.match(msg, line)
-    print(type(match))
-    print(match)
-    print("\n\n")
-    print("########################################################\n")
-
-    if not match:
-        return None
-
-    paso(11)
+    
     try:
-        paso(12)
         event = json.loads(match.group(1))
-        paso(18)
         if (event['type'] == 'event' and
                 event['data']['type'] == 'deleted' and
                 'process_name' not in event['data']['audit']):
-            paso(13)
             return event
-        paso(14)
-    # except (AttributeError, JSONDecodeError, KeyError):
-    #     paso(15)
-    #     pass
-    # paso(16)
-    # return None
-    except (JSONDecodeError, AttributeError, KeyError) as e:
-        paso(15)
-        logger.warning(f"Couldn't load a log line into json object. Reason {e}")
-    paso(16)
+    except (AttributeError, JSONDecodeError, KeyError):
+        pass
+
+    return None
+    
 # fixtures
 
 
 @pytest.fixture(scope='module', params=configurations)
 def get_configuration(request):
     """Get configurations from the module."""
-    paso(17)
     return request.param
 
 
@@ -117,51 +90,48 @@ def test_deferred_delete_file(folder, file_list, filetype, tags_to_apply,
     filetype : str
         Type of the files that will be created.
     """
-    paso(0)
+    
     check_apply_test(tags_to_apply, get_configuration['tags'])
     print(type(tags_to_apply))
-    paso(1)
+
     # Create files inside subdir folder
     for file in file_list:
         create_file(filetype, folder, file, content='')
-    paso(2)
+
     # Wait for the added events
     wazuh_log_monitor.start(timeout=global_parameters.default_timeout, callback=callback_detect_event,
                             accum_results=len(file_list), error_message='Did not receive expected '
                             '"Sending FIM event: ..." event')
-    paso(3)
+
     # Delete the files under 'folder'
     command = 'del "{}"\n'.format(folder)
-    paso(4)
+
     cmd = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=DEVNULL, universal_newlines=True)
     try:
         stdout = cmd.communicate(timeout=global_parameters.default_timeout)
-        paso(5)
     except TimeoutError:
-        paso(6)
         pass
 
     # Find the windows confirmation character
     confirmation = re.search(r'\((\w)\/\w\)\?', stdout[0])
     assert confirmation
-    paso(7)
     # Run the command again and this time delete the files
     cmd = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=DEVNULL, universal_newlines=True)
     try:
         stdout = cmd.communicate('{}\n'.format(confirmation.group(1)), timeout=global_parameters.default_timeout)
-        paso(8)
     except TimeoutError:
-        paso(9)
         pass
 
+    print("\n#####################################\n")
+    print(global_parameters.default_timeout)
+    print("\n")
+    print(len(file_list))
+    print("\n")
+    print('Did not receive expected '
+                            '"Sending FIM event: ..." event')
+    print("\n#####################################\n")
+
     # Start monitoring
-    # wazuh_log_monitor.start(timeout=global_parameters.default_timeout, callback=callback_detect_delete_event,
-    #                         accum_results=len(file_list), error_message='Did not receive expected '
-    #                         '"Sending FIM event: ..." event')
-    # paso(10)
-
-
-def paso(nro):
-    print("\n\n")
-    print("################## - PASO " + str(nro) + " - ##################")
-    print("\n\n")
+    wazuh_log_monitor.start(timeout=global_parameters.default_timeout*10, callback=callback_detect_delete_event,
+                            accum_results=len(file_list), error_message='Did not receive expected '
+                            '"Sending FIM event: ..." event')
