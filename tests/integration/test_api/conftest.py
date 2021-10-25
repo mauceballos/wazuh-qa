1de6227e26d46d8f6b63f6087adf5c2e61411b28
fedepacher <fede_pacher@hotmail.com>
2021-10-25 12:58:36 -0300

    change start to restart

--- tests/integration/test_api/conftest.py
+++ tests/integration/test_api/conftest.py
@@ -84,9 +84,9 @@ def restart_api(get_configuration, request):
     setattr(request.module, 'wazuh_log_monitor', file_monitor)
 
     # Start Wazuh API
-    for process_name in ['wazuh-apid', 'wazuh-modulesd', 'wazuh-analysisd', 'wazuh-execd', 'wazuh-db', 'wazuh-remoted', 
-    'wazuh-monitord', 'wazuh-logcollector', 'wazuh-syscheckd', 'wazuh-authd',
-    'wazuh-clusterd', 'wazuh-maild', 'wazuh-agentlessd', 'wazuh-integratord', 'wazuh-dbd', 'wazuh-csyslogd']:
+    for process_name in ['wazuh-apid', 'wazuh-modulesd', 'wazuh-analysisd', 'wazuh-execd', 'wazuh-db', 'wazuh-remoted']:#, 
+    # 'wazuh-monitord', 'wazuh-logcollector', 'wazuh-syscheckd', 'wazuh-authd',
+    # 'wazuh-clusterd', 'wazuh-maild', 'wazuh-agentlessd', 'wazuh-integratord', 'wazuh-dbd', 'wazuh-csyslogd']:
         control_service('restart', daemon=process_name)
     #control_service('start')
 
