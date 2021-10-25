3446ccda5ee9d898b1037d1a3380317da1f2e406
fedepacher <fede_pacher@hotmail.com>
2021-10-25 15:56:46 -0300

    test_admin_resources original

--- tests/integration/test_api/test_rbac/test_admin_resources.py
+++ tests/integration/test_api/test_rbac/test_admin_resources.py
@@ -13,16 +13,12 @@ def get_configuration():
 
 # Functions
 def get_admin_resources(api_details, endpoint):
-    print(f"{api_details['base_url']}{endpoint}")
-    print("\n################################################\n")
-    print(headers=api_details['auth_headers'])
-    # response = requests.get(f"{api_details['base_url']}{endpoint}", headers=api_details['auth_headers'], verify=False)
-    # assert response.status_code == 200, f'Expected status code was 200. Full response: {response.text}'
+    response = requests.get(f"{api_details['base_url']}{endpoint}", headers=api_details['auth_headers'], verify=False)
+    assert response.status_code == 200, f'Expected status code was 200. Full response: {response.text}'
 
-    # admin_ids = [item['id'] for item in response.json()['data']['affected_items'] if item['id'] < 100]
+    admin_ids = [item['id'] for item in response.json()['data']['affected_items'] if item['id'] < 100]
 
-    # return admin_ids
-    return None
+    return admin_ids
 
 
 def remove_admin_resources(api_details, admin_ids, endpoint, resource, exception):
@@ -85,50 +81,50 @@ def test_admin_users(restart_api, get_api_details):
     """Test if admin security users can be removed."""
     api_details = get_api_details()
 
-    # endpoint = '/security/users'
-    # resource = 'user_ids'
-    # admin_ids = get_admin_resources(api_details, endpoint)
-    #remove_admin_resources(api_details, admin_ids, endpoint, resource, 5004)
+    endpoint = '/security/users'
+    resource = 'user_ids'
+    admin_ids = get_admin_resources(api_details, endpoint)
+    remove_admin_resources(api_details, admin_ids, endpoint, resource, 5004)
 
 
-# @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
-# def test_admin_roles(restart_api, get_api_details):
-#     """Test if admin security roles can be removed."""
-#     api_details = get_api_details()
+@pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
+def test_admin_roles(restart_api, get_api_details):
+    """Test if admin security roles can be removed."""
+    api_details = get_api_details()
 
-#     endpoint = '/security/roles'
-#     resource = 'role_ids'
-#     body = {'name': 'random_role_name_test'}
+    endpoint = '/security/roles'
+    resource = 'role_ids'
+    body = {'name': 'random_role_name_test'}
 
-#     admin_ids = get_admin_resources(api_details, endpoint)
-#     remove_admin_resources(api_details, admin_ids, endpoint, resource, 4008)
-#     modify_admin_resources(api_details, admin_ids, endpoint, body)
-
-
-# @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
-# def test_admin_policies(restart_api, get_api_details):
-#     """Test if admin security policies can be removed."""
-#     api_details = get_api_details()
-
-#     endpoint = '/security/policies'
-#     resource = 'policy_ids'
-#     body = {'name': 'random_policy_name_test',
-#             'policy': {'actions': ['test_action'], 'resources': ['test_resources'], 'effect': 'allow'}}
-
-#     admin_ids = get_admin_resources(api_details, endpoint)
-#     remove_admin_resources(api_details, admin_ids, endpoint, resource, 4008)
-#     modify_admin_resources(api_details, admin_ids, endpoint, body)
+    admin_ids = get_admin_resources(api_details, endpoint)
+    remove_admin_resources(api_details, admin_ids, endpoint, resource, 4008)
+    modify_admin_resources(api_details, admin_ids, endpoint, body)
 
 
-# @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
-# def test_admin_rules(restart_api, get_api_details):
-#     """Test if admin security rules can be removed."""
-#     api_details = get_api_details()
+@pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
+def test_admin_policies(restart_api, get_api_details):
+    """Test if admin security policies can be removed."""
+    api_details = get_api_details()
 
-#     endpoint = '/security/rules'
-#     resource = 'rule_ids'
-#     body = {'name': 'random_rule_name_test', 'rule': {'rule_key': 'rule_value'}}
+    endpoint = '/security/policies'
+    resource = 'policy_ids'
+    body = {'name': 'random_policy_name_test',
+            'policy': {'actions': ['test_action'], 'resources': ['test_resources'], 'effect': 'allow'}}
 
-#     admin_ids = get_admin_resources(api_details, endpoint)
-#     remove_admin_resources(api_details, admin_ids, endpoint, resource, 4008)
-#     modify_admin_resources(api_details, admin_ids, endpoint, body)
+    admin_ids = get_admin_resources(api_details, endpoint)
+    remove_admin_resources(api_details, admin_ids, endpoint, resource, 4008)
+    modify_admin_resources(api_details, admin_ids, endpoint, body)
+
+
+@pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
+def test_admin_rules(restart_api, get_api_details):
+    """Test if admin security rules can be removed."""
+    api_details = get_api_details()
+
+    endpoint = '/security/rules'
+    resource = 'rule_ids'
+    body = {'name': 'random_rule_name_test', 'rule': {'rule_key': 'rule_value'}}
+
+    admin_ids = get_admin_resources(api_details, endpoint)
+    remove_admin_resources(api_details, admin_ids, endpoint, resource, 4008)
+    modify_admin_resources(api_details, admin_ids, endpoint, body)
