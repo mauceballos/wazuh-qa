8d7fdc8a5f0e25b2ab17528a8e24694868f32eac
mauromalara <mauro.malara@wazuh.com>
2021-12-02 12:18:56 -0300

    Modify: Thread library changed

--- tests/performance/test_analysisd/test_analysisd.py
+++ tests/performance/test_analysisd/test_analysisd.py
@@ -4,9 +4,8 @@ import pytest
 import yaml
 import socket
 from sys import path
-from wazuh_testing.tools.thread_executor import ThreadExecutor
+from threading import Thread
 import wazuh_testing.tools as tools
-from wazuh_testing.tools import file
 from wazuh_testing.tools.performance.binary import Monitor
 from deps.wazuh_testing.wazuh_testing.tools import ANALYSISD_QUEUE_SOCKET_PATH
 
@@ -23,29 +22,31 @@ with open(ingestion_rates_path) as test_cases_file:
                          ids=[test_case['name'] for test_case in test_cases])
 def test_analysisd(test_cases):
     for scenario in test_cases:
-        msg = '0912:tmpdir:Mensaje'
+        # Creating a path for results
         if os.path.exists(f"{test_results_path}/{scenario['eps']}"):
             sh.rmtree(f"{test_results_path}/{scenario['eps']}", ignore_errors=True)
         os.mkdir(f"{test_results_path}/{scenario['eps']}")
         monitor = Monitor(process_name='analysisd', dst_dir=f'{test_results_path}/{scenario["eps"]}')
+        
+        msg = '0912:tmpdir:Mensaje'
+        
         def send_message(message, wazuh_socket):
-
             sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
             sock.connect(wazuh_socket)
             sock.send(message.encode())
             sock.close()
 
-        events_threads = []
+        threads = []
+
+        for i in range(0, scenario['eps']):
+            threads.append(Thread(target=send_message, args=(msg, ANALYSISD_QUEUE_SOCKET_PATH)))
 
         monitor.start()
 
-        for event in range(0, scenario['eps']):
-            events_threads.append(ThreadExecutor(send_message, {'message': msg, 'wazuh_socket': ANALYSISD_QUEUE_SOCKET_PATH}))
-
-       	for thread in events_threads:
+       	for thread in threads:
             thread.start()
         
-        for thread in events_threads:
+        for thread in threads:
             thread.join()
 
         monitor.shutdown()
