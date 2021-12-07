95b5f68feca16425d59a1ffe67c596dc3ab39d8a
mauromalara <mauro.malara@wazuh.com>
2021-12-07 13:03:21 -0300

    Add: Calculate the EPS dropped and decoded average.

--- tests/performance/test_analysisd/injector.py
+++ tests/performance/test_analysisd/injector.py
@@ -18,14 +18,10 @@ class Injector():
         self.events_per_thread = self.eps // self.quantity_of_threads
         self.total_events_decoded = []
         self.total_events_dropped = []
-        self.start_injector(float(execution_time))
+        self.execution_time = float(execution_time)
 
     def detect_events_decoded(self, time_limit):
-        drops_not_detected = True
-        time_limit = float(time_limit)
-        elapsed_time = 0.0
-        while drops_not_detected:
-            start = time.time()
+        while True:
             cfg = ConfigObj(ANALYSIS_STATISTICS_FILE)
             total_events_decoded = int(cfg['total_events_decoded'])
             events_dropped = int(cfg['events_dropped'])
@@ -35,14 +31,12 @@ class Injector():
                 self.total_events_decoded.append(total_events_decoded)
             if events_dropped > 0:
                 self.total_events_dropped.append(events_dropped)
-                drops_not_detected = False
-
-            end = time.time()
-            elapsed_time = elapsed_time + (end - start)
-            print(elapsed_time, end='\n')
-            if elapsed_time >= time_limit: # Stop the thread if elapsed time reach the time limit
-                drops_not_detected = False
+            if events_dropped > 1000:
+                return True
+            time_limit -= 1
             time.sleep(1)
+            if time_limit <= 0:
+                return False
 
     def send_message(self, message, wazuh_socket):
         for i in range(0, self.events_per_thread):
@@ -51,27 +45,25 @@ class Injector():
             sock.send(message.encode())
             sock.close()
 
-    def start_injector(self, expected_execution_time):
+    def start_injector(self):
         actual_execution_time = 0.0
-        while actual_execution_time < expected_execution_time:
-            start = time.time()
+        while actual_execution_time < self.execution_time:
+            start = time.perf_counter()
+
             threads = []
-            for i in range(0, self.quantity_of_threads):
+            # Add the 'detector' thread
+            threads.append(Thread(target=self.detect_events_decoded, kwargs={'time_limit': self.execution_time}))
+
+            for i in range(0, self.quantity_of_threads): # Add the 'sender' threads
                 threads.append(Thread(target=self.send_message, args=(msg, ANALYSISD_QUEUE_SOCKET_PATH)))
-                if i == 0:
-                    threads.append(Thread(target=self.detect_events_decoded, kwargs={'time_limit': expected_execution_time}))
-                    threads[i+1].start()
 
             for i, thread in enumerate(threads):
-                if i == 1:
-                    continue
-                else:
-                    thread.start()
+                thread.start()
 
             for thread in threads:
-                thread.join(timeout=10)
-            end = time.time()
-            actual_execution_time += end - start
+                thread.join()
 
+            finish = time.perf_counter()
+            actual_execution_time += round(finish - start, 2)
 
         return self.total_events_decoded, self.total_events_dropped
