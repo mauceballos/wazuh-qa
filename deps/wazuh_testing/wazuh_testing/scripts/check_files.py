7c884639f33b1cae003b8ee25c5643288e612d98
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-12-27 10:30:23 +0100

    Try to find out which files cant retrieve its gid

--- deps/wazuh_testing/wazuh_testing/scripts/check_files.py
+++ deps/wazuh_testing/wazuh_testing/scripts/check_files.py
@@ -176,7 +176,10 @@ def get_data_information(item):
     """
     stat_info = os.stat(item)
     user = pwd.getpwuid(stat_info.st_uid)[0]
-    group = grp.getgrgid(stat_info.st_gid)[0]
+    try:
+        group = grp.getgrgid(stat_info.st_gid)[0]
+    except KeyError:
+        return {'group keyerror'}
     mode = oct(stat.S_IMODE(stat_info.st_mode))
     mode_str = str(mode).replace('o', '')
     mode = mode_str[-3:] if len(mode_str) > 3 else mode_str
