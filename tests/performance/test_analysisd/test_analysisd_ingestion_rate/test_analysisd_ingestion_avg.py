1e94b0e22ba9de1a81e5d6a80c657aa2c1eeb59c
mauromalara <mauro.malara@wazuh.com>
2021-12-30 14:32:35 -0300

    Add: Test documentation & Fix: Assert condition fixed & Del: Unused fixture deleted.

--- tests/performance/test_analysisd/test_analysisd_ingestion_rate/test_analysisd_ingestion_avg.py
+++ tests/performance/test_analysisd/test_analysisd_ingestion_rate/test_analysisd_ingestion_avg.py
@@ -1,20 +1,86 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+'''
+copyright: Copyright (C) 2015-2021, Wazuh Inc.
 
-import json
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: performance
+
+brief: The 'wazuh-analysisd' daemon receives a log message and if it matches
+       an applicable rule then the daemon creates an alert. The analysisd queue
+       socket receives the message and sends it to the respective decoder
+       queue, and if the selected queue is full the event is dropped.
+       This test aims to compare the average of dropped events before and after
+       the upgrade of the manager, and checks if the ingestion rate does
+       not decrease.
+
+tier: 0
+
+modules:
+    - analysisd
+
+components:
+    - manager
+
+daemons:
+    - wazuh-analysisd
+
+os_platform:
+    - linux
+    - windows
+    - macos
+    - solaris
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+    - Windows 10
+    - Windows 8
+    - Windows 7
+    - Windows Server 2019
+    - Windows Server 2016
+    - Windows Server 2012
+    - Windows Server 2003
+    - Windows XP
+    - macOS Catalina
+    - Solaris 10
+    - Solaris 11
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/reference/daemons/wazuh-analysisd.html
+
+tags:
+    - analysisd_performance
+    - analysisd_ingestion_rate
+'''
+
 import pytest
 import os
 
 from wazuh_testing.tools.file import validate_json_file, read_json_file
 from wazuh_testing.tools.file import write_json_file
-from wazuh_testing.tools.file import recursive_directory_creation
 
 
 @pytest.fixture
 def get_first_result(request):
     """Allow to use the --before-file parameter in order to pass the results
-    before upgrade Wazuh.
+    path before upgrading Wazuh.
 
     Args:
         request (fixture): Provide information on the executing test function.
@@ -25,7 +91,7 @@ def get_first_result(request):
 @pytest.fixture
 def get_second_result(request):
     """Allow to use the --after-file parameter in order to pass the results
-    after upgrade Wazuh.
+    path after upgrading Wazuh.
 
     Args:
         request (fixture): Provide information on the executing test function.
@@ -35,23 +101,13 @@ def get_second_result(request):
 
 @pytest.fixture
 def get_output_path(request):
-    """Allow to use the --output-path parameter to store the test result in the
-    specified file.
+    """Allow to use the --output-path parameter to store the test result in
+    the specified file.
 
     Args:
         request (fixture): Provide information on the executing test function.
     """
     return request.config.getoption('--output-path')
-
-
-@pytest.fixture
-def get_ingestion_rate(request):
-    """Allow to use the --ingestion-rate parameter.
-
-    Args:
-        request (fixture): Provide information on the executing test function.
-    """
-    return request.config.getoption('--ingestion-rate')
 
 
 def validate_and_read_json(file_path):
@@ -92,17 +148,41 @@ def validate_and_create_output_path(output_path):
 
 def test_analysisd_ingestion_rate(get_first_result, get_second_result,
                                   get_output_path):
-    """This test checks if the performance of Analysisd decreased after
-    upgrade Wazuh.
+    '''
+    description: This test aims to compare the average of dropped events before and after
+                 the upgrade of the manager, and checks if the ingestion rate does
+                 not decrease.
 
-    Args:
-        get_first_result (fixture): Get the results file before making any
-        changes to the environment.
-        get_second_result (fixture): Get the results file after making any
-        changes to the environment.
-        get_output_path (fixture): Get the output path where the result will
-        be saved.
-    """
+    wazuh_min_version: 4.2.0
+
+    parameters:
+        - get_first_result:
+            type: fixture
+            brief: Get the file path of the results before upgrading Wazuh.
+        - get_second_result:
+            type: fixture
+            brief: Get the file path of the results after upgrading Wazuh.
+        - get_output_path:
+            type: fixture
+            brief: Get the file path where the test result will be stored.
+    
+    assertions:
+        - Verify that the ingestion rate does not decreased after upgrading
+          Wazuh.
+
+    input_description: The results of stress analysisd, before and after
+                       upgrading Wazuh, are stored in 2 files. They contain
+                       the necessary data for the test to compare them.
+
+    expected_output:
+        - A JSON with the result of the test.
+        - The ingestion rate decreased after the upgrade,
+          check the results within /path/to/result/file
+
+    tags:
+        - analysisd_performance
+        - ingestion_rate
+    '''
     file1_data = validate_and_read_json(get_first_result)
     file2_data = validate_and_read_json(get_second_result)
     validate_and_create_output_path(get_output_path)
@@ -110,7 +190,7 @@ def test_analysisd_ingestion_rate(get_first_result, get_second_result,
     initial_drop_average = int(file1_data['Average']['Dropped'])
     final_drop_average = int(file2_data['Average']['Dropped'])
 
-    threshold = -20
+    threshold = 20
 
     if initial_drop_average == final_drop_average:
         drop_variation = 0
@@ -143,6 +223,6 @@ def test_analysisd_ingestion_rate(get_first_result, get_second_result,
     results_path = os.path.join(get_output_path, 'test_results.json')
     write_json_file(results_path, result_data)
 
-    if drop_variation <= threshold:
-        assert False, f"The ingestion rate decreased after the upgrade, \
-                      check the results within {get_output_path}"
+    assert not drop_variation >= threshold, "The ingestion rate decreased" \
+                                            "after the upgrade," \
+                                            f"check the results within {get_output_path}"
