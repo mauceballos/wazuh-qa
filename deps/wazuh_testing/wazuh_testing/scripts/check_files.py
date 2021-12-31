de19fd1b015a011936865d6e3dc76bbb670ba6e7
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-12-31 14:11:36 +0100

    Get file that does not retrieve its gid

--- deps/wazuh_testing/wazuh_testing/scripts/check_files.py
+++ deps/wazuh_testing/wazuh_testing/scripts/check_files.py
@@ -129,13 +129,13 @@ def get_check_files_data(path='/', ignored_paths=[]):
                     try:
                         files_items_dict[dirpath] = get_data_information(dirpath)
                     except OSError:  # Ignore errors like "No such device or address" due to dynamic and temporary files
-                        files_items_dict[dirpath] = f"First temp attempt {dirpath}"
-
+                        pass
+ 
                 if file_path not in ignored_paths and os.path.exists(file_path):
                     try:
                         files_items_dict[file_path] = get_data_information(file_path)
                     except OSError:  # Ignore errors like "No such device or address" due to dynamic and temporary files
-                        files_items_dict[dirpath] = f"Second temp attempt {dirpath}"
+                        pass
 
     return files_items_dict
 
@@ -176,7 +176,10 @@ def get_data_information(item):
     """
     stat_info = os.stat(item)
     user = pwd.getpwuid(stat_info.st_uid)[0]
-    group = grp.getgrgid(stat_info.st_gid)[0]
+    try:
+        group = grp.getgrgid(stat_info.st_gid)[0]
+    except KeyError:
+        return {'keyerror': 'gid'}
     mode = oct(stat.S_IMODE(stat_info.st_mode))
     mode_str = str(mode).replace('o', '')
     mode = mode_str[-3:] if len(mode_str) > 3 else mode_str
